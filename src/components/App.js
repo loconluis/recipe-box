07682e1089522ec01b43179d3ef4261c8dd448c5
loconluis <luis.locon@gmail.com>
2018-01-12 20:23:08 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Box</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/components/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
